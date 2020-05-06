b9cd9d8b20eaa9538c42ea7231d03e7b6e359e79
Cule219 <culaficstefan@gmail.com>
2020-05-06 15:55:42 -0400

    react-rev1

---
+++ starter_code/client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Express Cinema</h1>
+      </div>
+    );
+  }
+}
