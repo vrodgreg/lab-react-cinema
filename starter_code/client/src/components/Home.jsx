0c49d81c2141d9f20bc9d33d78b14a258b21fa2c
Cule219 <culaficstefan@gmail.com>
2020-05-08 12:19:35 -0400

    Bonus added

--- starter_code/client/src/components/Home.jsx
+++ starter_code/client/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Home extends Component {
+  async callMyBackend() {
+    // this is an example call to our backend
+    let responseFromBackend = await axios.get("http://localhost:5000/");
+    console.log(responseFromBackend);
+  }
   render() {
     return (
       <div>
+        <button onClick={this.callMyBackend}>Test Backend Call</button>
         <h1>Welcome to Express Cinema</h1>
       </div>
     );
--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  console.log("called");
   res.json({ message: "index" });
 });
 
